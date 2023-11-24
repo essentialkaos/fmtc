d1031a193eb3780ca92bdeb746ddccf20e6f1e4f
Anton Novojilov <andy@essentialkaos.com>
2023-11-24 16:54:39 +0300

    The very first version

--- README.md
+++ README.md
@@ -1,2 +1,101 @@
-# fmtc
-Simple utility for rendering fmtc formatted data
+<p align="center"><a href="#readme"><img src="https://gh.kaos.st/fmtc.svg"/></a></p>
+
+<p align="center">
+  <a href="https://kaos.sh/r/fmtc"><img src="https://kaos.sh/r/fmtc.svg" alt="GoReportCard" /></a>
+  <a href="https://kaos.sh/w/fmtc/ci"><img src="https://kaos.sh/w/fmtc/ci.svg" alt="GitHub Actions CI Status" /></a>
+  <a href="https://kaos.sh/w/fmtc/codeql"><img src="https://kaos.sh/w/fmtc/codeql.svg" alt="GitHub Actions CodeQL Status" /></a>
+  <a href="#license"><img src="https://gh.kaos.st/apache2.svg"></a>
+</p>
+
+<p align="center"><a href="#installation">Installation</a> • <a href="#command-line-completion">Command-line completion</a> • <a href="#man-documentation">Man documentation</a> • <a href="#usage">Usage</a> • <a href="#ci-status">CI Status</a> • <a href="#contributing">Contributing</a> • <a href="#license">License</a></p>
+
+<br/>
+
+`fmtc` is a simple utility for rendering [fmtc](https://github.com/essentialkaos/ek/tree/master/fmtc#readme) formatted data. You can use it instead of the `echo` command to print colored messages.
+
+```bash
+# Simple formatted message
+fmtc "{*}Done!{!} File {#87}$file{!} successfully uploaded to {g_}$host{!}"
+
+# Use option -E/--error to print message to stderr
+fmtc -E "{r*}There is no user bob{!}"
+
+# You can use stdin as a source of data
+echo "{*}Done!{!} File {#87}$file{!} successfully uploaded to {g_}$host{!}" | fmtc
+# or
+fmtc <<< "{*}Done!{!} File {#87}$file{!} successfully uploaded to {g_}$host{!}"
+
+# You can print message without colors using -nc/--no-color option
+fmtc -nc "{*}Done!{!} File {#87}$file{!} successfully uploaded to {g_}$host{!}"
+
+# Also, fmtc supports the NO_COLOR environment variable (https://no-color.org)
+NO_COLOR=1 fmtc "{*}Done!{!} File {#87}$file{!} successfully uploaded to {g_}$host{!}"
+```
+
+### Installation
+
+#### From source
+
+To build the `fmtc` from scratch, make sure you have a working Go 1.20+ workspace (_[instructions](https://go.dev/doc/install)_), then:
+
+```
+go install github.com/essentialkaos/fmtc@latest
+```
+
+#### Prebuilt binaries
+
+You can download prebuilt binaries for Linux and macOS from [EK Apps Repository](https://apps.kaos.st/fmtc/latest):
+
+```bash
+bash <(curl -fsSL https://apps.kaos.st/get) fmtc
+```
+
+### Command-line completion
+
+You can generate completion for `bash`, `zsh` or `fish` shell.
+
+Bash:
+```bash
+sudo fmtc --completion=bash 1> /etc/bash_completion.d/fmtc
+```
+
+ZSH:
+```bash
+sudo fmtc --completion=zsh 1> /usr/share/zsh/site-functions/fmtc
+```
+
+Fish:
+```bash
+sudo fmtc --completion=fish 1> /usr/share/fish/vendor_completions.d/fmtc.fish
+```
+
+### Man documentation
+
+You can generate man page using next command:
+
+```bash
+fmtc --generate-man | sudo gzip > /usr/share/man/man1/fmtc.1.gz
+```
+
+### Usage
+
+```
+
+```
+
+### CI Status
+
+| Branch | Status |
+|--------|----------|
+| `master` | [![CI](https://kaos.sh/w/fmtc/ci.svg?branch=master)](https://kaos.sh/w/fmtc/ci?query=branch:master) |
+| `develop` | [![CI](https://kaos.sh/w/fmtc/ci.svg?branch=develop)](https://kaos.sh/w/fmtc/ci?query=branch:develop) |
+
+### Contributing
+
+Before contributing to this project please read our [Contributing Guidelines](https://github.com/essentialkaos/contributing-guidelines#contributing-guidelines).
+
+### License
+
+[Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+<p align="center"><a href="https://essentialkaos.com"><img src="https://gh.kaos.st/ekgh.svg"/></a></p>
---
+++ fmtc.go
@@ -0,0 +1,281 @@
+package main
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                         Copyright (c) 2023 ESSENTIAL KAOS                          //
+//      Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>     //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"runtime"
+
+	_ "embed"
+
+	"github.com/essentialkaos/ek/v12/fmtc"
+	"github.com/essentialkaos/ek/v12/fsutil"
+	"github.com/essentialkaos/ek/v12/options"
+	"github.com/essentialkaos/ek/v12/usage"
+	"github.com/essentialkaos/ek/v12/usage/completion/bash"
+	"github.com/essentialkaos/ek/v12/usage/completion/fish"
+	"github.com/essentialkaos/ek/v12/usage/completion/zsh"
+	"github.com/essentialkaos/ek/v12/usage/man"
+
+	"github.com/essentialkaos/fmtc/support"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// Basic utility info
+const (
+	APP  = "fmtc"
+	VER  = "0.0.1"
+	DESC = "Utility for rendering fmtc formatted data"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// Options
+const (
+	OPT_ERROR    = "E:error"
+	OPT_LINE     = "L:line"
+	OPT_NO_COLOR = "nc:no-color"
+	OPT_HELP     = "h:help"
+	OPT_VER      = "v:version"
+
+	OPT_VERB_VER     = "vv:verbose-version"
+	OPT_COMPLETION   = "completion"
+	OPT_GENERATE_MAN = "generate-man"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// optMap contains information about all supported options
+var optMap = options.Map{
+	OPT_ERROR:    {Type: options.BOOL},
+	OPT_LINE:     {Type: options.BOOL},
+	OPT_NO_COLOR: {Type: options.BOOL},
+	OPT_HELP:     {Type: options.BOOL},
+	OPT_VER:      {Type: options.MIXED},
+
+	OPT_VERB_VER:     {Type: options.BOOL},
+	OPT_COMPLETION:   {},
+	OPT_GENERATE_MAN: {Type: options.BOOL},
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+//go:embed go.mod
+var gomod []byte
+
+// gitrev is short hash of the latest git commit
+var gitRev string
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// colorTagApp is tag used for app name
+var colorTagApp string
+
+// colorTagVer is tag used for app version
+var colorTagVer string
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// main is main utility function
+func main() {
+	runtime.GOMAXPROCS(1)
+
+	preConfigureUI()
+
+	args, errs := options.Parse(optMap)
+
+	if len(errs) != 0 {
+		printError(errs[0].Error())
+		os.Exit(1)
+	}
+
+	configureUI()
+
+	switch {
+	case options.Has(OPT_COMPLETION):
+		os.Exit(printCompletion())
+	case options.Has(OPT_GENERATE_MAN):
+		printMan()
+		os.Exit(0)
+	case options.GetB(OPT_VER):
+		genAbout(gitRev).Print(options.GetS(OPT_VER))
+		os.Exit(0)
+	case options.GetB(OPT_VERB_VER):
+		support.Print(APP, VER, gitRev, gomod)
+		os.Exit(0)
+	case options.GetB(OPT_HELP):
+		genUsage().Print()
+		os.Exit(0)
+	}
+
+	colorData(args)
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// preConfigureUI preconfigures UI based on information about user terminal
+func preConfigureUI() {
+	if os.Getenv("NO_COLOR") != "" {
+		fmtc.DisableColors = true
+	}
+}
+
+// configureUI configures user interface
+func configureUI() {
+	if options.GetB(OPT_NO_COLOR) {
+		fmtc.DisableColors = true
+	}
+
+	switch {
+	case fmtc.IsTrueColorSupported():
+		colorTagApp, colorTagVer = "{*}{#FF1D7C}", "{#FF1D7C}"
+	case fmtc.Is256ColorsSupported():
+		colorTagApp, colorTagVer = "{*}{#197}", "{#197}"
+	default:
+		colorTagApp, colorTagVer = "{*}{m}", "{m}"
+	}
+}
+
+// colorData processes input data
+func colorData(args options.Arguments) {
+	var data string
+	var isStdin bool
+
+	if len(args) != 0 {
+		data = args.Flatten()
+	} else {
+		if !fsutil.IsCharacterDevice("/dev/stdin") {
+			stdinData, err := io.ReadAll(os.Stdin)
+
+			if err != nil {
+				os.Exit(1)
+			}
+
+			data = string(stdinData)
+			isStdin = true
+		} else {
+			os.Exit(1)
+		}
+	}
+
+	if options.GetB(OPT_LINE) || isStdin {
+		if options.GetB(OPT_ERROR) {
+			fmtc.Fprint(os.Stderr, data)
+		} else {
+			fmtc.Fprint(os.Stdout, data)
+		}
+	} else {
+		if options.GetB(OPT_ERROR) {
+			fmtc.Fprintln(os.Stderr, data)
+		} else {
+			fmtc.Fprintln(os.Stdout, data)
+		}
+	}
+}
+
+// printError prints error message to console
+func printError(f string, a ...interface{}) {
+	if len(a) == 0 {
+		fmtc.Fprintln(os.Stderr, "{r}"+f+"{!}")
+	} else {
+		fmtc.Fprintf(os.Stderr, "{r}"+f+"{!}\n", a...)
+	}
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// printCompletion prints completion for given shell
+func printCompletion() int {
+	info := genUsage()
+
+	switch options.GetS(OPT_COMPLETION) {
+	case "bash":
+		fmt.Printf(bash.Generate(info, "fmtc"))
+	case "fish":
+		fmt.Printf(fish.Generate(info, "fmtc"))
+	case "zsh":
+		fmt.Printf(zsh.Generate(info, optMap, "fmtc"))
+	default:
+		return 1
+	}
+
+	return 0
+}
+
+// printMan prints man page
+func printMan() {
+	fmt.Println(
+		man.Generate(
+			genUsage(),
+			genAbout(""),
+		),
+	)
+}
+
+// genUsage generates usage info
+func genUsage() *usage.Info {
+	info := usage.NewInfo("", "data…")
+
+	info.AppNameColorTag = colorTagApp
+
+	info.AddOption(OPT_ERROR, "Print data to stderr")
+	info.AddOption(OPT_LINE, "Don't print newline at the end")
+	info.AddOption(OPT_NO_COLOR, "Disable colors in output")
+	info.AddOption(OPT_HELP, "Show this help message")
+	info.AddOption(OPT_VER, "Show version")
+
+	info.AddExample(
+		`"{*}Done!{!} File {#87}$file{!} successfully uploaded to {g_}$host{!}"`,
+		"Print fmtc formatted message",
+	)
+
+	info.AddExample(
+		`-E "{r*}There is no user bob{!}"`,
+		"Print fmtc formatted message to stderr",
+	)
+
+	info.AddExample(
+		`-nc "{*}Done!{!} File {#87}$file{!} successfully uploaded to {g_}$host{!}"`,
+		"Print message without colors using -nc/--no-color option",
+	)
+
+	info.AddRawExample(
+		`echo "{*}Done!{!} File {#87}$file{!} successfully uploaded to {g_}$host{!}" | fmtc`,
+		"Use stdin as a source of data",
+	)
+
+	return info
+}
+
+// genAbout generates info about version
+func genAbout(gitRev string) *usage.About {
+	about := &usage.About{
+		App:     APP,
+		Version: VER,
+		Desc:    DESC,
+		Year:    2009,
+		Owner:   "ESSENTIAL KAOS",
+		License: "Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>",
+
+		AppNameColorTag: colorTagApp,
+		VersionColorTag: colorTagVer,
+
+		DescSeparator: "—",
+	}
+
+	if gitRev != "" {
+		about.Build = "git:" + gitRev
+	}
+
+	return about
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
